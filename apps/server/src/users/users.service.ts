a22328bb6be861cdf4cf5820c2524958915117de
Umang Sailor <umang@weetechsolution.com>
2026-05-11 10:32:27 +0530

    refactor: replace bcrypt with bcryptjs for password hashing
    - Updated users.service.ts to import bcryptjs instead of bcrypt.
    - Modified pnpm-lock.yaml to remove bcrypt and add bcryptjs.
    - Adjusted pnpm-workspace.yaml to exclude bcrypt from onlyBuiltDependencies.

--- apps/server/src/users/users.service.ts
+++ apps/server/src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
